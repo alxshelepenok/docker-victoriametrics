eec50d4f22b9d8d9e3ea82fff1635b92e5598634
Alexander Shelepenok <alxshelepenok@gmail.com>
2023-01-18 19:49:13 +0000

    chore: publish docker images to registry

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 
 set -ex
 IMAGE_NAME="waterscape/victoriametrics"
+NEXUS_REPOSITORY="nexus.docker-hosted.wtrscape.com"
 TAG="${1}"
 docker build -t ${IMAGE_NAME}:"${TAG}" .
+docker tag ${IMAGE_NAME}:"${TAG}" ${NEXUS_REPOSITORY}/${IMAGE_NAME}:"${TAG}"
+docker push ${NEXUS_REPOSITORY}/${IMAGE_NAME}:"${TAG}"
