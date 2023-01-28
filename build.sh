a5f2e73ed57331f602e53ca139b3edf84831ef94
Alexander Shelepenok <alxshelepenok@gmail.com>
2023-01-28 11:00:45 +0000

    chore: change nexus hostnames

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 IMAGE_NAME="waterscape/victoriametrics"
-NEXUS_REPOSITORY="nexus.docker-hosted.wtrscape.com"
+NEXUS_REPOSITORY="docker-hosted.nexus.wtrscape.com"
 TAG="${1}"
 docker build -t ${IMAGE_NAME}:"${TAG}" .
 docker tag ${IMAGE_NAME}:"${TAG}" ${NEXUS_REPOSITORY}/${IMAGE_NAME}:"${TAG}"
